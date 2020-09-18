7cd651aa9fb1244c121557449dd8f383507d488c
Olafhu9 <olafhu9@hanyang.ac.kr>
2020-09-18 13:41:57 +0900

    modi

--- ttbb/ana_ttbb.C
+++ ttbb/ana_ttbb.C
@@ -80,10 +80,10 @@ int count_matched(rvec_i Idx, rvec_f jet_pt, rvec_f jet_eta, rvec_f jet_phi, rve
 }
 
 //********** matched jet index
-rvec_i matchedjet(rvec_i Idx, rvec_f jet_pt, rvec_f jet_eta, rvec_f jet_phi, rvec_f jet_e, TLorentzVector addbjet){
+vector <int> matchedjet(rvec_i Idx, rvec_f jet_pt, rvec_f jet_eta, rvec_f jet_phi, rvec_f jet_e, TLorentzVector addbjet){
     
     TLorentzVector jet;
-    rvec_i matchedjet_idx;
+    vector <int> matchedjet_idx;
     float dR;
 
     for(int i = 0; i < 4; i++){
@@ -96,130 +96,131 @@ rvec_i matchedjet(rvec_i Idx, rvec_f jet_pt, rvec_f jet_eta, rvec_f jet_phi, rve
 }
 
 //********** matched jet pt
-rvec_f matchedjet_pt(rvec_i matchedjet_idx, rvec_f jet_pt, rvec_f jet_eta, rvec_f jet_phi, rvec_f jet_e){
-    cout << "***************************" << endl; 
+vector <float> matchedjet_pt(vector <int> matchedjet_idx, rvec_f jet_pt, rvec_f jet_eta, rvec_f jet_phi, rvec_f jet_e){
     TLorentzVector jet;
-    rvec_f Pt;
+    vector <float> Pt;
     float pt; 
     for (int i = 0; i < matchedjet_idx.size(); i++){
         jet.SetPtEtaPhiE(jet_pt[matchedjet_idx[i]], jet_eta[matchedjet_idx[i]], jet_phi[matchedjet_idx[i]], jet_e[matchedjet_idx[i]]);
         pt = jet.Pt();
-        cout << "pt : " << pt << endl;
         Pt.push_back(pt);
-    }
-    for (int i = 0; i < Pt.size(); i++){
-        cout << i << " " << Pt[i] << endl;
     }
     return Pt;
 }
 
 //*********** matched jet  deltaR
-rvec_f matchedjet_dR(rvec_i matchedjet_idx, rvec_f jet_pt, rvec_f jet_eta, rvec_f jet_phi, rvec_f jet_e, TLorentzVector addbjet){
-    cout << "***************************" << endl; 
+vector <float> matchedjet_dR(vector <int> matchedjet_idx, rvec_f jet_pt, rvec_f jet_eta, rvec_f jet_phi, rvec_f jet_e, TLorentzVector addbjet){
     TLorentzVector jet;
-    rvec_f deltaR;
+    vector <float> deltaR;
     float dR;
     for (int i = 0; i < matchedjet_idx.size(); i++){
         jet.SetPtEtaPhiE(jet_pt[matchedjet_idx[i]], jet_eta[matchedjet_idx[i]], jet_phi[matchedjet_idx[i]], jet_e[matchedjet_idx[i]]);
         dR = jet.DeltaR(addbjet);
-        cout << "dR : " << dR << endl;
         deltaR.push_back(dR);
     }
-    for (int i = 0; i < deltaR.size(); i++){
-        cout << i << " " << deltaR[i] << endl;
-    }
-    return deltaR;
-    }
+    return deltaR;    
+}
+
+int getsize(vector <int> vec){
+    int size = vec.size();
+//    cout << "*************************" << endl;
+//    cout << "size: " << size << endl;
+//    for (int i = 0; i < size; i++)
+//        cout << i << " " << vec[i] << endl;
+    return size;
+}
+
 
 void ana_ttbb(){
   ROOT::RDataFrame df("ttbbLepJets/tree", "/cms/ldap_home/sarakm0704/public/ttbb/V10_3/sync/TTLJ_PowhegPythia_ttbb.root");
 
-  //*** Lepton cut
-  auto df_goodlepton = df.Filter("lepton_pt > 30 && abs(lepton_eta) < 2.4")
-                         .Define("goodjets","jet_pt > 20 && abs(jet_eta) < 2.4")
-                         .Define("ngoodjets", "Sum(goodjets)")
-                         .Define("bjets_m", "jet_deepJet > 0.277 && jet_pt > 20 && abs(jet_eta) < 2.4")
-                         .Define("bjets_t", "jet_deepJet > 0.7264 && jet_pt > 20 && abs(jet_eta) < 2.4")
-                         .Define("nbjets_t", "Sum(bjets_t)")
-                         .Define("nbjets_m", "Sum(bjets_m)");
-  //*** nJets >= 4
-  auto df_goodjet = df_goodlepton.Filter("ngoodjets >=4 ", "Events with at least 4 goodjets");
-  auto df_bjet = df_goodjet.Filter("nbjets_m >= 2", "Events with at least 2 medium b-jets")
-                         //*** sort btag
-                         .Define("sortjets", sortbtag, {"jet_pt", "jet_eta", "jet_deepJet"})
-                         //*** additional b-jets
-                         .Define("addbjet1", jet_vector, {"addbjet1_pt", "addbjet1_eta", "addbjet1_phi", "addbjet1_e"})
-                         .Define("addbjet2", jet_vector, {"addbjet2_pt", "addbjet2_eta", "addbjet2_phi", "addbjet2_e"})
-                         //*** count # of mathed jets
-                         .Define("count", count_matched, {"sortjets", "jet_pt", "jet_eta", "jet_phi", "jet_e", "jet_deepJet",
-                                                         "addbjet1_pt", "addbjet1_eta", "addbjet1_phi", "addbjet1_e",
-                                                         "addbjet2_pt", "addbjet2_eta", "addbjet2_phi", "addbjet2_e"})
-                         //*** matched jet index
-                         .Define("matched_add1_idx", matchedjet, {"sortjets", "jet_pt", "jet_eta", "jet_phi", "jet_e", "addbjet1"})
-                         .Define("matched_add2_idx", matchedjet, {"sortjets", "jet_pt", "jet_eta", "jet_phi", "jet_e", "addbjet2"})
+    //*** Lepton cut
+    auto df_goodlepton = df.Filter("lepton_pt > 30 && abs(lepton_eta) < 2.4")
+                           .Define("goodjets","jet_pt > 20 && abs(jet_eta) < 2.4")
+                           .Define("ngoodjets", "Sum(goodjets)")
+                           .Define("bjets_m", "jet_deepJet > 0.277 && jet_pt > 20 && abs(jet_eta) < 2.4")
+                           .Define("bjets_t", "jet_deepJet > 0.7264 && jet_pt > 20 && abs(jet_eta) < 2.4")
+                           .Define("nbjets_t", "Sum(bjets_t)")
+                           .Define("nbjets_m", "Sum(bjets_m)");
+    //*** nJets >= 4
+    auto df_goodjet = df_goodlepton.Filter("ngoodjets >=4 ", "Events with at least 4 goodjets");
+    auto df_bjet = df_goodjet.Filter("nbjets_m >= 2", "Events with at least 2 medium b-jets")
+                           //*** sort btag
+                           .Define("sortjets", sortbtag, {"jet_pt", "jet_eta", "jet_deepJet"})
+                           //*** additional b-jets
+                           .Define("addbjet1", jet_vector, {"addbjet1_pt", "addbjet1_eta", "addbjet1_phi", "addbjet1_e"})
+                           .Define("addbjet2", jet_vector, {"addbjet2_pt", "addbjet2_eta", "addbjet2_phi", "addbjet2_e"})
+                           //*** count # of mathed jets
+                           .Define("count", count_matched, {"sortjets", "jet_pt", "jet_eta", "jet_phi", "jet_e", "jet_deepJet",
+                                                            "addbjet1_pt", "addbjet1_eta", "addbjet1_phi", "addbjet1_e",
+                                                            "addbjet2_pt", "addbjet2_eta", "addbjet2_phi", "addbjet2_e"})
+                           //*** matched jet index
+                           .Define("matched_add1_idx", matchedjet, {"sortjets", "jet_pt", "jet_eta", "jet_phi", "jet_e", "addbjet1"})
+                           .Define("matched_add2_idx", matchedjet, {"sortjets", "jet_pt", "jet_eta", "jet_phi", "jet_e", "addbjet2"})
+                           .Define("nmatched1", getsize, {"matched_add1_idx"})
+                           .Define("nmatched2", getsize, {"matched_add2_idx"})
+
                          
-                         .Define("matchedjet_pt1", matchedjet_pt, {"matched_add1_idx", "jet_pt", "jet_eta", "jet_phi", "jet_e"})
-                         .Define("matchedjet_pt2", matchedjet_pt, {"matched_add2_idx", "jet_pt", "jet_eta", "jet_phi", "jet_e"})
-                         .Define("matchedjet_dR1", matchedjet_dR, {"matched_add1_idx", "jet_pt", "jet_eta", "jet_phi", "jet_e", "addbjet1"})
-                         .Define("matchedjet_dR2", matchedjet_dR, {"matched_add2_idx", "jet_pt", "jet_eta", "jet_phi", "jet_e", "addbjet2"})
+                           .Define("matchedjet_pt1", matchedjet_pt, {"matched_add1_idx", "jet_pt", "jet_eta", "jet_phi", "jet_e"})
+                           .Define("matchedjet_pt2", matchedjet_pt, {"matched_add2_idx", "jet_pt", "jet_eta", "jet_phi", "jet_e"})
+                           .Define("matchedjet_dR1", matchedjet_dR, {"matched_add1_idx", "jet_pt", "jet_eta", "jet_phi", "jet_e", "addbjet1"})
+                           .Define("matchedjet_dR2", matchedjet_dR, {"matched_add2_idx", "jet_pt", "jet_eta", "jet_phi", "jet_e", "addbjet2"})
 
 
-                         //*** jet1
-                         .Define("jet1_pt", "jet_pt[sortjets[0]]")
-                         .Define("jet1_eta", "jet_eta[sortjets[0]]")
-                         .Define("jet1_phi", "jet_phi[sortjets[0]]")
-                         .Define("jet1_e", "jet_e[sortjets[0]]")
-                         .Define("jet1_btag", "jet_deepJet[sortjets[0]]")
-                         .Define("jet1_CvsB", "jet_deepJetCvsB[sortjets[0]]")
-                         .Define("jet1_CvsL", "jet_deepJetCvsL[sortjets[0]]")
-                         //*** jet2
-                         .Define("jet2_pt", "jet_pt[sortjets[1]]")
-                         .Define("jet2_eta", "jet_eta[sortjets[1]]")
-                         .Define("jet2_phi", "jet_phi[sortjets[1]]")
-                         .Define("jet2_e", "jet_e[sortjets[1]]")
-                         .Define("jet2_btag", "jet_deepJet[sortjets[1]]")
-                         .Define("jet2_CvsB", "jet_deepJetCvsB[sortjets[1]]")
-                         .Define("jet2_CvsL", "jet_deepJetCvsL[sortjets[1]]")
-                         //*** jet3
-                         .Define("jet3_pt", "jet_pt[sortjets[2]]")
-                         .Define("jet3_eta", "jet_eta[sortjets[2]]")
-                         .Define("jet3_phi", "jet_phi[sortjets[2]]")
-                         .Define("jet3_e", "jet_e[sortjets[2]]")
-                         .Define("jet3_btag", "jet_deepJet[sortjets[2]]")
-                         .Define("jet3_CvsB", "jet_deepJetCvsB[sortjets[2]]")
-                         .Define("jet3_CvsL", "jet_deepJetCvsL[sortjets[2]]")
-                         //*** jet4
-                         .Define("jet4_pt", "jet_pt[sortjets[3]]")
-                         .Define("jet4_eta", "jet_eta[sortjets[3]]")
-                         .Define("jet4_phi", "jet_phi[sortjets[3]]")
-                         .Define("jet4_e", "jet_e[sortjets[3]]")
-                         .Define("jet4_btag", "jet_deepJet[sortjets[3]]")
-                         .Define("jet4_CvsB", "jet_deepJetCvsB[sortjets[3]]")
-                         .Define("jet4_CvsL", "jet_deepJetCvsL[sortjets[3]]");
-//  auto df_cat0 = df_bjet.Filter("category == 0");
-//  auto df_cat1 = df_bjet.Filter("category == 1");
-//  auto df_cat2 = df_bjet.Filter("category == 2");
-//  auto df_cat3 = df_bjet.Filter("category == 3");
-//  auto df_cat4 = df_bjet.Filter("category == 4");
-//  auto df_cat5 = df_bjet.Filter("category == 5");
-//  auto df_cat6 = df_bjet.Filter("category == 6");
+                           //*** jet1
+                           .Define("jet1_pt", "jet_pt[sortjets[0]]")
+                           .Define("jet1_eta", "jet_eta[sortjets[0]]")
+                           .Define("jet1_phi", "jet_phi[sortjets[0]]")
+                           .Define("jet1_e", "jet_e[sortjets[0]]")
+                           .Define("jet1_btag", "jet_deepJet[sortjets[0]]")
+                           .Define("jet1_CvsB", "jet_deepJetCvsB[sortjets[0]]")
+                           .Define("jet1_CvsL", "jet_deepJetCvsL[sortjets[0]]")
+                           //*** jet2
+                           .Define("jet2_pt", "jet_pt[sortjets[1]]")
+                           .Define("jet2_eta", "jet_eta[sortjets[1]]")
+                           .Define("jet2_phi", "jet_phi[sortjets[1]]")
+                           .Define("jet2_e", "jet_e[sortjets[1]]")
+                           .Define("jet2_btag", "jet_deepJet[sortjets[1]]")
+                           .Define("jet2_CvsB", "jet_deepJetCvsB[sortjets[1]]")
+                           .Define("jet2_CvsL", "jet_deepJetCvsL[sortjets[1]]")
+                           //*** jet3
+                           .Define("jet3_pt", "jet_pt[sortjets[2]]")
+                           .Define("jet3_eta", "jet_eta[sortjets[2]]")
+                           .Define("jet3_phi", "jet_phi[sortjets[2]]")
+                           .Define("jet3_e", "jet_e[sortjets[2]]")
+                           .Define("jet3_btag", "jet_deepJet[sortjets[2]]")
+                           .Define("jet3_CvsB", "jet_deepJetCvsB[sortjets[2]]")
+                           .Define("jet3_CvsL", "jet_deepJetCvsL[sortjets[2]]")
+                           //*** jet4
+                           .Define("jet4_pt", "jet_pt[sortjets[3]]")
+                           .Define("jet4_eta", "jet_eta[sortjets[3]]")
+                           .Define("jet4_phi", "jet_phi[sortjets[3]]")
+                           .Define("jet4_e", "jet_e[sortjets[3]]")
+                           .Define("jet4_btag", "jet_deepJet[sortjets[3]]")
+                           .Define("jet4_CvsB", "jet_deepJetCvsB[sortjets[3]]")
+                           .Define("jet4_CvsL", "jet_deepJetCvsL[sortjets[3]]");
 
-  cout << "hi" << endl;
+    auto df_matched1 = df_bjet.Filter("nmatched1 > 0", "Events with matched jet with add1");
+    auto df_matched2 = df_bjet.Filter("nmatched2 > 0", "Events with matched jet with add2");
 
+    cout << "hi" << endl;
 
+    auto h_matched1 = df_matched1.Histo2D({"h_matched1", "h_matched1", 100, 0, 0.4, 180, 20, 200}, "matchedjet_dR1", "matchedjet_pt1");
+    auto h_matched2 = df_matched2.Histo2D({"h_matched2", "h_matched2", 100, 0, 0.4, 180, 20, 200}, "matchedjet_dR2", "matchedjet_pt2"); 
   //snapshot
   ROOT::RDF::RSnapshotOptions opts;
   opts.fMode = "UPDATE";
   
-  df_bjet.Snapshot("dnn_input", "ttbb_pt20_test3.root", {"count", "matched_add1_idx", "matched_add2_idx", "matchedjet_pt1", "matchedjet_pt2", "matchedjet_dR1", "matchedjet_dR2"});
-  //df_bjet.Snapshot("dnn_input", "ttbb_cnt2.root", {"count", "jet1_pt", "jet1_eta", "jet1_btag", "jet1_e", "jet2_pt", "jet2_eta", "jet2_btag", "jet2_e", "jet3_pt", "jet3_eta", "jet3_btag", "jet3_e", "jet4_pt", "jet4_eta", "jet4_btag", "jet4_e", "deltaR_12", "deltaR_13", "deltaR_14", "deltaR_23", "deltaR_24", "deltaR_34", "lepton_pt", "lepton_eta", "lepton_e", "deltaR_lepj1", "deltaR_lepj2", "deltaR_lepj3", "deltaR_lepj4", "invmass_12", "invmass_13", "invmass_14", "invmass_23", "invmass_24", "invmass_34", "invmass_lepj1", "invmass_lepj2", "invmass_lepj3", "invmass_lepj4", "jet1_CvsB", "jet1_CvsL", "jet2_CvsB", "jet2_CvsL", "jet3_CvsB", "jet3_CvsL", "jet4_CvsB", "jet4_CvsL"});
-//  df_bjet.Snapshot("dnn_input", "ttbb_ctag.root", {"signal", "category", "jet1_pt", "jet1_eta", "jet1_btag", "jet1_e", "jet2_pt", "jet2_eta", "jet2_btag", "jet2_e", "jet3_pt", "jet3_eta", "jet3_btag", "jet3_e", "jet4_pt", "jet4_eta", "jet4_btag", "jet4_e", "deltaR_12", "deltaR_13", "deltaR_14", "deltaR_23", "deltaR_24", "deltaR_34", "lepton_pt", "lepton_eta", "lepton_e", "deltaR_lepj1", "deltaR_lepj2", "deltaR_lepj3", "deltaR_lepj4", "invmass_12", "invmass_13", "invmass_14", "invmass_23", "invmass_24", "invmass_34", "invmass_lepj1", "invmass_lepj2", "invmass_lepj3", "invmass_lepj4", "jet1_CvsB", "jet1_CvsL", "jet2_CvsB", "jet2_CvsL", "jet3_CvsB", "jet3_CvsL", "jet4_CvsB", "jet4_CvsL"});
-//  df_cat0.Snapshot("catetory_0", "ttbb_invmasstest.root", {"category", "jet1_pt", "jet1_eta", "jet1_btag", "jet1_e", "jet2_pt", "jet2_eta", "jet2_btag", "jet2_e", "jet3_pt", "jet3_eta", "jet3_btag", "jet3_e", "jet4_pt", "jet4_eta", "jet4_btag", "jet4_e", "deltaR_12", "deltaR_13", "deltaR_14", "deltaR_23", "deltaR_24", "deltaR_34", "lepton_pt", "lepton_eta", "lepton_e", "deltaR_lepj1", "deltaR_lepj2", "deltaR_lepj3", "deltaR_lepj4"}, opts);
-//  df_cat1.Snapshot("catetory_1", "ttbb_invmasstest.root", {"category", "jet1_pt", "jet1_eta", "jet1_btag", "jet1_e", "jet2_pt", "jet2_eta", "jet2_btag", "jet2_e", "jet3_pt", "jet3_eta", "jet3_btag", "jet3_e", "jet4_pt", "jet4_eta", "jet4_btag", "jet4_e", "deltaR_12", "deltaR_13", "deltaR_14", "deltaR_23", "deltaR_24", "deltaR_34", "lepton_pt", "lepton_eta", "lepton_e", "deltaR_lepj1", "deltaR_lepj2", "deltaR_lepj3", "deltaR_lepj4"}, opts);
-//  df_cat2.Snapshot("catetory_2", "ttbb_invmasstest.root", {"category", "jet1_pt", "jet1_eta", "jet1_btag", "jet1_e", "jet2_pt", "jet2_eta", "jet2_btag", "jet2_e", "jet3_pt", "jet3_eta", "jet3_btag", "jet3_e", "jet4_pt", "jet4_eta", "jet4_btag", "jet4_e", "deltaR_12", "deltaR_13", "deltaR_14", "deltaR_23", "deltaR_24", "deltaR_34", "lepton_pt", "lepton_eta", "lepton_e", "deltaR_lepj1", "deltaR_lepj2", "deltaR_lepj3", "deltaR_lepj4"}, opts);
-//  df_cat3.Snapshot("catetory_3", "ttbb_invmasstest.root", {"category", "jet1_pt", "jet1_eta", "jet1_btag", "jet1_e", "jet2_pt", "jet2_eta", "jet2_btag", "jet2_e", "jet3_pt", "jet3_eta", "jet3_btag", "jet3_e", "jet4_pt", "jet4_eta", "jet4_btag", "jet4_e", "deltaR_12", "deltaR_13", "deltaR_14", "deltaR_23", "deltaR_24", "deltaR_34", "lepton_pt", "lepton_eta", "lepton_e", "deltaR_lepj1", "deltaR_lepj2", "deltaR_lepj3", "deltaR_lepj4"}, opts);
-//  df_cat4.Snapshot("catetory_4", "ttbb_invmasstest.root", {"category", "jet1_pt", "jet1_eta", "jet1_btag", "jet1_e", "jet2_pt", "jet2_eta", "jet2_btag", "jet2_e", "jet3_pt", "jet3_eta", "jet3_btag", "jet3_e", "jet4_pt", "jet4_eta", "jet4_btag", "jet4_e", "deltaR_12", "deltaR_13", "deltaR_14", "deltaR_23", "deltaR_24", "deltaR_34", "lepton_pt", "lepton_eta", "lepton_e", "deltaR_lepj1", "deltaR_lepj2", "deltaR_lepj3", "deltaR_lepj4"}, opts);
-//  df_cat5.Snapshot("catetory_5", "ttbb_invmasstest.root", {"category", "jet1_pt", "jet1_eta", "jet1_btag", "jet1_e", "jet2_pt", "jet2_eta", "jet2_btag", "jet2_e", "jet3_pt", "jet3_eta", "jet3_btag", "jet3_e", "jet4_pt", "jet4_eta", "jet4_btag", "jet4_e", "deltaR_12", "deltaR_13", "deltaR_14", "deltaR_23", "deltaR_24", "deltaR_34", "lepton_pt", "lepton_eta", "lepton_e", "deltaR_lepj1", "deltaR_lepj2", "deltaR_lepj3", "deltaR_lepj4"}, opts);
-//  df_cat6.Snapshot("catetory_6", "ttbb_invmasstest.root", {"category", "jet1_pt", "jet1_eta", "jet1_btag", "jet1_e", "jet2_pt", "jet2_eta", "jet2_btag", "jet2_e", "jet3_pt", "jet3_eta", "jet3_btag", "jet3_e", "jet4_pt", "jet4_eta", "jet4_btag", "jet4_e", "deltaR_12", "deltaR_13", "deltaR_14", "deltaR_23", "deltaR_24", "deltaR_34", "lepton_pt", "lepton_eta", "lepton_e", "deltaR_lepj1", "deltaR_lepj2", "deltaR_lepj3", "deltaR_lepj4"}, opts);
+  //df_bjet.Snapshot("dnn_input", "ttbb_pt20_test5.root", {"nmatched1", "count", "matched_add1_idx", "matched_add2_idx"});
+    df_bjet.Snapshot("dnn_input", "ttbb_pt20_test6.root", {"nmatched1", "nmatched2", "count", "matched_add1_idx", "matched_add2_idx", "matchedjet_pt1", "matchedjet_pt2", "matchedjet_dR1", "matchedjet_dR2"});
+    df_matched1.Snapshot("add1", "ttbb_pt20_test6.root", {"count", "matched_add1_idx", "matched_add2_idx", "matchedjet_pt1", "matchedjet_pt2", "matchedjet_dR1", "matchedjet_dR2"}, opts);
+    df_matched2.Snapshot("add2", "ttbb_pt20_test6.root", {"count", "matched_add1_idx", "matched_add2_idx", "matchedjet_pt1", "matchedjet_pt2", "matchedjet_dR1", "matchedjet_dR2"}, opts);
 
+
+    TFile f("ttbb_pt20_test6.root", "UPDATE");
+
+        plot(h_matched1, "h_matched1");
+        plot(h_matched2, "h_matched2");
+
+    f.Close();
 }
